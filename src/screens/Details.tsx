c4ebd59582952daba4783ac1e0e54ce6b91528e6
vts1978 <vinicioussouza@gmail.com>
2022-07-19 04:55:25 -0300

    class 02

---
+++ src/screens/Details.tsx
@@ -0,0 +1,23 @@
+import { useRoute } from '@react-navigation/native';
+
+import { VStack, Text } from 'native-base';
+import { Header } from '../components/Header';
+
+type RoutesParams = {
+    orderId: string;
+}
+
+export function Details() {
+    const route = useRoute();
+    const { orderId } = route.params as RoutesParams;
+
+
+    return (
+        <VStack flex={1} bg="gray.700">
+            <Header title="Solicitação" />
+            <Text color="white">
+                {orderId}
+            </Text>
+        </VStack>
+    );
+}
